c2c00b12ac743fbe36b14882e3f7e8e8b2a7093b
thdevnit <108213563+thdevnit@users.noreply.github.com>
2023-03-24 15:02:44 +0530

    Update myDetails.sol

--- Contracts/myDetails.sol
+++ Contracts/myDetails.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.0;
 contract myDetails {
     uint256 private age;
 
-    function setDetails(string memory _age) public {
+    function setDetails(uint256 _age) public {
         age = _age;
     }
 
-    function getAge() public view returns (string memory) {
+    function getAge() public view returns (uint256) {
         return age;
     }
 }
